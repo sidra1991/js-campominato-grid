dcf7cb979fa7526f8dce98734f3d933113ad039b
sidra1991 <1991sidra@gmail.com>
2022-03-02 15:50:36 +0100

    appunti

--- funzioni/script.js
+++ funzioni/script.js
@@ -5,12 +5,22 @@ L'utente indica un livello di difficoltà in base al quale viene generata una gr
 con difficoltà 1 => tra 1 e 100
 con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
-
 */
+
+// richiesta del utente di attivare la funzione
+
+    // raccogli scelta utente di difficoltà /-/ i valori sono già indicati nel input html quindi questo dato sarà sfruttato nel if.
+
+
+    // indenta le 3 diverse scelte (tanto sono identiche varia solo il numero) + 1 in cui l'utente sceglie il suo numero a scelta libera . /-/ contengono il ciclo per generare le caselle che verranno inserite nel html da js.
+
 
 /*
 Lasciate perdere il cambiamento del colore della cella (serve una cosa che oggi non abbiamo visto). Qundi le celle saranno tutte dello stesso colore e non succede niente quando le clicchiamo.
 */
+
+// provaci lo stesso
+
 
 /*
 Consigli del giorno: :party_wizard:
