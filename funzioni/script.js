da1c4bb2337bd6e6c859264a9f16c92aa7fba02a
sidra1991 <1991sidra@gmail.com>
2022-03-02 15:36:36 +0100

    consegna in js

---
+++ funzioni/script.js
@@ -0,0 +1,23 @@
+
+/*
+Consegna
+L'utente indica un livello di difficoltà in base al quale viene generata una griglia di gioco quadrata, in cui ogni cella contiene un numero (in ordine) tra quelli compresi in un range:
+con difficoltà 1 => tra 1 e 100
+con difficoltà 2 => tra 1 e 81
+con difficoltà 3 => tra 1 e 49
+
+*/
+
+/*
+Lasciate perdere il cambiamento del colore della cella (serve una cosa che oggi non abbiamo visto). Qundi le celle saranno tutte dello stesso colore e non succede niente quando le clicchiamo.
+*/
+
+/*
+Consigli del giorno: :party_wizard:
+Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
+Ad esempio:
+Di cosa ho bisogno per generare i numeri?
+Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
+Le validazioni e i controlli possiamo farli anche in un secondo momento.
+------------------------------------------------------------
+non mi piacciono i consol.log -.-
