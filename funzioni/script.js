12ee6f066f6fbac59776f66625c38d7f085ad6bb
sidra1991 <1991sidra@gmail.com>
2022-03-03 18:55:20 +0100

    genero bombe

--- funzioni/script.js
+++ funzioni/script.js
@@ -7,23 +7,22 @@ con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
 */
 
- const contenitore = document.getElementById(`contenitore`)
-
+    const contenitore = document.getElementById(`contenitore`)
+    let campo = [];
+    let bombe = [];
 // richiesta del utente di attivare la funzione
 function generatore(){
     let celle = document.getElementById(`difficolta`).value;
     contenitore.innerHTML='';
-    console.log(`log delle celle ${celle}`)
-    
+  
+    campo = [];
 
     // indenta le 3 diverse scelte (tanto sono identiche varia solo il numero) . /-/ contengono il ciclo per generare le caselle che verranno inserite nel html da js.
 
-    for (let generatore = 1; generatore < celle; generatore++) {
+    for (let generatore = 1; generatore  < celle ; generatore++) {
         
 
         const cella = document.createElement(`div`);
-
-        console.log(generatore)
 
         cella.innerHTML = `${generatore}`;
 
@@ -33,24 +32,34 @@ function generatore(){
             cella.style.backgroundColor=`blue`            
         } )
         
-        if (parseInt(celle) === 100) {
+        if (parseInt(celle) === 101) {
             cella.classList.add("facile");
             cella.classList.remove("medio");
             cella.classList.remove("difficile")
-        } else if (parseInt(celle) === 80) {
+        } else if (parseInt(celle) === 81) {
             cella.classList.add("medio");
             cella.classList.remove("facile");
             cella.classList.remove("difficile")
-        } else if ( parseInt(celle) === 50 ){
+        } else if ( parseInt(celle) === 51 ){
             cella.classList.add("difficile");
             cella.classList.remove("medio");
             cella.classList.remove("facile")
         }
-        
+        campo.push(cella)
+
+    }    
+
+    bombe = [];
+    for (let bomb = 0; bombe.length  < 16; bomb++) {
+        let bomba = Math.floor(Math.random() * campo.length);
+        console.log(bomba)
+        bombe.push(`${bomba}`);
+   
     }
-    
+        
+  
 
-
+    console.log(`queste so le bombe ${bombe}`)
 }
 
 /*
