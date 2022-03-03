e8000c20fbeb113a75ecae7ed985f7ff67d2ff14
sidra1991 <1991sidra@gmail.com>
2022-03-04 00:33:19 +0100

    cambiato tutto di nuovo, 3 caselel per vincere (prova)

--- funzioni/script.js
+++ funzioni/script.js
@@ -6,81 +6,68 @@ con difficoltà 1 => tra 1 e 100
 con difficoltà 2 => tra 1 e 81
 con difficoltà 3 => tra 1 e 49
 */
-
+    let vittoria = 0;
     const contenitore = document.getElementById(`contenitore`)
     let campo = [];
     let bombe = [];
 // richiesta del utente di attivare la funzione
 function generatore(){
-    let celle = document.getElementById(`difficolta`).value;
+    let difficolta = document.getElementById(`difficolta`).value;
     contenitore.innerHTML='';
-  
     campo = [];
-
+    bombe = [];
+    vittoria = 0;
     // indenta le 3 diverse scelte (tanto sono identiche varia solo il numero) . /-/ contengono il ciclo per generare le caselle che verranno inserite nel html da js.
 
-    for (let generatore = 1; generatore  < celle ; generatore++) {
-        
+    for (let generatore = 0; generatore  < difficolta ; generatore++) {
+       let cella = document.createElement(`div.cella`);
+        if (generatore < 16) {
+            cella.addEventListener(`click`, function () {
+                cella.style.backgroundColor = `red`;
+                alert("hai perso")   
+                window.location.reload()       
+            } )
+        } else {
+            cella.addEventListener(`click`, function () {
+                cella.style.backgroundColor = `blue` ;   
+                
+                vittoria = vittoria + 1;
+                    if (vittoria > 3) {
+                     alert("ai vinto")
+                    }
+            } );
+        };
 
-        const cella = document.createElement(`div`);
-
-        cella.innerHTML = `${generatore}`;
-
-        contenitore.append(cella)
-
-        cella.addEventListener(`click`, function() {
-            cella.style.backgroundColor=`blue`            
-        } )
-        
-        if (parseInt(celle) === 101) {
+        if (parseInt(difficolta) === 101) {
             cella.classList.add("facile");
             cella.classList.remove("medio");
             cella.classList.remove("difficile")
-        } else if (parseInt(celle) === 81) {
+        } else if (parseInt(difficolta) === 81) {
             cella.classList.add("medio");
             cella.classList.remove("facile");
             cella.classList.remove("difficile")
-        } else if ( parseInt(celle) === 51 ){
+        } else if ( parseInt(difficolta) === 51 ){
             cella.classList.add("difficile");
             cella.classList.remove("medio");
             cella.classList.remove("facile")
         }
-        campo.push(cella)
-
+        campo.push(cella);
     }    
 
-    bombe = [];
-    for (let bomb = 0; bombe.length  < 16; bomb++) {
-        let bomba = Math.floor(Math.random() * campo.length);
-        console.log(bomba)
-        
-        if (bombe.includes(bomba,0) == false) {
-          bombe.push(bomba)  
+    
+    let controllo = []
+
+    for (let stampa = 1; controllo.length < campo.length ; stampa++) {
+        let sparso = Math.floor(Math.random() * (campo.length));
+        const casella = campo[sparso];
+        console.log(sparso)
+        if (controllo.includes(sparso) == false) {
+            controllo.push(sparso);
+            contenitore.append(casella)
+            casella.innerHTML = `${stampa}`;
         }
-
-        campo.splice( bomba,0,bomba )
     }
-        
-  
 
-    console.log(`queste so le bombe ${bombe}`)
+
+
 }
-
-/*
-Lasciate perdere il cambiamento del colore della cella (serve una cosa che oggi non abbiamo visto). Qundi le celle saranno tutte dello stesso colore e non succede niente quando le clicchiamo.
-*/
-
-// provaci lo stesso
-
-
-/*
-Consigli del giorno: :party_wizard:
-Scriviamo prima cosa vogliamo fare passo passo in italiano, dividiamo il lavoro in micro problemi.
-Ad esempio:
-Di cosa ho bisogno per generare i numeri?
-Proviamo sempre prima con dei console.log() per capire se stiamo ricevendo i dati giusti.
-Le validazioni e i controlli possiamo farli anche in un secondo momento.
-------------------------------------------------------------
-non mi piacciono i consol.log -.-
-
-*/
